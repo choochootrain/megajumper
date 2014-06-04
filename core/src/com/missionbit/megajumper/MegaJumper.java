958d7950c509e2fb5c038565cf3c2f6c927770eb
Mission Bit <missionbit@lightcoral-option.local>
2014-06-03 20:26:45 -0700

    clean up imports

--- core/src/com/missionbit/megajumper/MegaJumper.java
+++ core/src/com/missionbit/megajumper/MegaJumper.java
@@ -3,9 +3,7 @@ package com.missionbit.megajumper;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 import java.util.ArrayList;
